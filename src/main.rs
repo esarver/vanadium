c87a834236455d00ede072293fb207e130bd62b7
Edwin Sarver <edwin.l.sarver@gmail.com>
2021-03-02 13:52:26 -0500

    Add Terminal per tui docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use termion::raw::IntoRawMode;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    Ok(())
 }
