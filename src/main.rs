09f8c9c381a197ca155428334f91c86c6dd149f6
Sarver, Edwin <edwin.sarver@keithley.com>
2024-02-08 14:53:30 -0500

    Print file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::io;
-use tui::Terminal;
-use tui::backend::TermionBackend;
-use termion::raw::IntoRawMode;
+use std::{fs, path::PathBuf};
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+use clap::Parser;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum VanadiumErrors {
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The file to open in the editor
+    file: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let contents = fs::read_to_string(cli.file)?;
+
+    println!("{contents}");
 
     Ok(())
 }
