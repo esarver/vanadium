ff1ceb4f110bbef9bc2ea263c5047f3dbe50e851
Sarver, Edwin <edwin.sarver@keithley.com>
2024-02-08 15:23:28 -0500

    Render File to Alternate Screen

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{fs, path::PathBuf};
+use std::{fs, io::stdout, path::PathBuf};
 
 use clap::Parser;
 
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{backend::CrosstermBackend, widgets::Paragraph, Terminal};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -22,7 +28,28 @@ fn main() -> anyhow::Result<()> {
 
     let contents = fs::read_to_string(cli.file)?;
 
-    println!("{contents}");
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    'app_loop: loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(Paragraph::new(contents.clone()), area);
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break 'app_loop;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
 
     Ok(())
 }
